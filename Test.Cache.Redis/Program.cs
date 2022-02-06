6199e33df22a6441581a3ec89a1637eca05272d5
mrtagcaa@gmail.com <mrtagcaa@gmail.com>
2022-02-06 12:48:30 +0300

    redis cache test app added

---
+++ Test.Cache.Redis/Program.cs
@@ -0,0 +1,26 @@
+﻿using MixFRM.Cache.Redis;
+using System;
+
+namespace Test.Cache.Redis
+{
+    class Program
+    {
+        public Program()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            RedisCacheManager redisCacheManager = new RedisCacheManager();
+
+            string name = "test";
+            bool isSet = redisCacheManager.Set<string>("filozof", name);
+            string prs = redisCacheManager.Get<string>("filozof");
+            bool containsKey = redisCacheManager.Contains("filozof"); // => not real contains, run the equals command
+
+            //redisCacheManager.Remove("filozof");
+            //redisCacheManager.Clear(); //flush all cache servers
+
+        }
+    }
+}
